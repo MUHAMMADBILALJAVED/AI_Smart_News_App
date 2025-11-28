637035a7668f412e57e9ed7646bf793de386445a
MUHAMMAD BILAL JAVED <javedbilal2002@gmail.com>
2025-11-28 10:24:38 -0800

    Changes

--- lib/services/local_storage.dart
+++ lib/services/local_storage.dart
@@ -73,13 +73,30 @@
 //   }
 // }
 
-import 'dart:convert';
-import 'package:flutter/services.dart';
+//import 'dart:convert';
+//import 'package:flutter/services.dart';
 
-class LocalStorage {
-  static Future<List<Map<String, dynamic>>> fetchLocalNews() async {
-    final String response = await rootBundle.loadString('assets/all_articles.json');
-    final List<dynamic> data = json.decode(response);
-    return data.cast<Map<String, dynamic>>();
+//class LocalStorage {
+//  static Future<List<Map<String, dynamic>>> fetchLocalNews() async {
+//    final String response = await rootBundle.loadString('assets/all_articles.json');
+//    final List<dynamic> data = json.decode(response);
+//    return data.cast<Map<String, dynamic>>();
+//  }
+//}
+
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalService {
+
+  static Future<void> saveSummary(String articleId, String summary) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("summary_$articleId", summary);
   }
+
+  static Future<String?> getSavedSummary(String articleId) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("summary_$articleId");
+  }
+
 }
